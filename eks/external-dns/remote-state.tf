73b226666166b5432e4493596ed23f9bb038b3c0
Jeffrey Attoh <Jeffrey.attoh@octo.us>
2023-03-12 17:46:33 -0400

    Added EKS Module - Multi Repo

---
+++ eks/external-dns/remote-state.tf
@@ -0,0 +1,38 @@
+module "eks" {
+  source  = "cloudposse/stack-config/yaml//modules/remote-state"
+  version = "1.4.1"
+
+  component = var.eks_component_name
+
+  context = module.this.context
+}
+
+module "dns_gbl_delegated" {
+  source  = "cloudposse/stack-config/yaml//modules/remote-state"
+  version = "1.4.1"
+
+  component   = "dns-delegated"
+  environment = var.dns_gbl_delegated_environment_name
+
+  context = module.this.context
+
+  defaults = {
+    zones = {}
+  }
+}
+
+module "dns_gbl_primary" {
+  source  = "cloudposse/stack-config/yaml//modules/remote-state"
+  version = "1.4.1"
+
+  component   = "dns-primary"
+  environment = var.dns_gbl_primary_environment_name
+
+  context = module.this.context
+
+  ignore_errors = true
+
+  defaults = {
+    zones = {}
+  }
+}
